63af205b244e4ab1aaf9cec059106af1dc3eee9e
skreebydba <skreebydba@users.noreply.github.com>
2019-06-20 16:44:52 +0000

    Moved BEGIN TRAN

--- AcceleratedDatabaseRecovery/2 - AcceleratedTransaction.sql
+++ AcceleratedDatabaseRecovery/2 - AcceleratedTransaction.sql
@@ -6,9 +6,10 @@ WAITFOR TIME @waitfor;
 
 USE AcceleratedRecovery;
 GO
-DROP TABLE IF EXISTS dbo.fl1, dbo.fl2, dbo.fl3;
 
 BEGIN TRANSACTION
+DROP TABLE IF EXISTS dbo.fl1, dbo.fl2, dbo.fl3;
+
 SELECT s2.* INTO dbo.fl1 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
 SELECT s2.* INTO dbo.fl2 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
 SELECT s2.* INTO dbo.fl3 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
--- AcceleratedDatabaseRecovery/6 - AcceleratedAgTransaction.sql
+++ AcceleratedDatabaseRecovery/6 - AcceleratedAgTransaction.sql
@@ -8,7 +8,7 @@ USE AcceleratedAg;
 GO
 DROP TABLE IF EXISTS dbo.fl1, dbo.fl2, dbo.fl3;
 
-BEGIN TRANSACTION
+--BEGIN TRANSACTION
 SELECT s2.* INTO dbo.fl1 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
 SELECT s2.* INTO dbo.fl2 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
 SELECT s2.* INTO dbo.fl3 FROM sys.all_columns AS s1 CROSS JOIN sys.all_objects AS s2;
