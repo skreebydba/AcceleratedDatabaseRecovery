e3bcf503bda3c2736642a0f03677cd78839ec755
skreebydba <skreebydba@users.noreply.github.com>
2019-06-22 18:59:13 +0000

    Added comments with AG links

--- AcceleratedDatabaseRecovery/6 - AcceleratedAgTransaction.sql
+++ AcceleratedDatabaseRecovery/6 - AcceleratedAgTransaction.sql
@@ -1,3 +1,13 @@
+/* Steps for setting up an Availability Group environment can be found here 
+   This document references SQL Server 2012, but the steps will work for 
+   later versions https://www.brentozar.com/archive/2011/07/how-set-up-sql-server-denali-availability-groups/ 
+   
+   A tutorial for setting up an Availability Group on Azure VMs can be found here 
+   https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sql/virtual-machines-windows-portal-sql-availability-group-tutorial
+   
+   As with everything here, feel free to contact me with any questions. */
+
+
 DECLARE @waitfor DATETIME;
 
 SELECT @waitfor = CAST(DATEADD(SECOND, 10, CURRENT_TIMESTAMP) AS TIME);
